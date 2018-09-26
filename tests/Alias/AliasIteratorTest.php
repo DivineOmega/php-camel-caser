86ccc025a50233f188b47c5303bb7293f6685c65
Jan-Marten de Boer <janmarten.deboer@mediact.nl>
2018-09-26 12:47:12 +0200

    Test alias model and iterator
    Add unit tests to cover the alias model and corresponding iterator.

---
+++ tests/Alias/AliasIteratorTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace DivineOmega\CamelCaser\Tests\Alias;
+
+use DivineOmega\CamelCaser\Alias\AliasInterface;
+use PHPUnit\Framework\TestCase;
+use DivineOmega\CamelCaser\Alias\AliasIterator;
+
+/**
+ * @coversDefaultClass \DivineOmega\CamelCaser\Alias\AliasIterator
+ */
+class AliasIteratorTest extends TestCase
+{
+    /**
+     * @dataProvider aliasProvider
+     *
+     * @param AliasInterface ...$aliases
+     *
+     * @return void
+     *
+     * @covers ::__construct
+     * @covers ::current
+     */
+    public function testIterator(AliasInterface ...$aliases): void
+    {
+        $subject = new AliasIterator(...$aliases);
+
+        $this->assertInstanceOf(AliasIterator::class, $subject);
+        $this->assertEquals($aliases, iterator_to_array($subject));
+    }
+
+    /**
+     * @return array
+     */
+    public function aliasProvider(): array
+    {
+        return [
+            [],
+            [$this->createMock(AliasInterface::class)],
+            [
+                $this->createMock(AliasInterface::class),
+                $this->createMock(AliasInterface::class),
+                $this->createMock(AliasInterface::class)
+            ]
+        ];
+    }
+}
