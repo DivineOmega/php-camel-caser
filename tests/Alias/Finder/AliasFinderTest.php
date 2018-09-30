ee85a29a0e71d402970808ac460ff418273b3273
Jan-Marten de Boer <janmarten.deboer@mediact.nl>
2018-09-30 11:39:11 +0200

    Add alias finder test
    Add test to cover the alias finder.

---
+++ tests/Alias/Finder/AliasFinderTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace DivineOmega\CamelCaser\Tests\Alias\Finder;
+
+use DivineOmega\CamelCaser\Alias\AliasIteratorInterface;
+use DivineOmega\CamelCaser\Formatter\FunctionFormatterInterface;
+use DivineOmega\CamelCaser\Tests\CreateFunctionTrait;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use DivineOmega\CamelCaser\Alias\Finder\AliasFinder;
+use ReflectionFunctionAbstract;
+
+/**
+ * @coversDefaultClass \DivineOmega\CamelCaser\Alias\Finder\AliasFinder
+ */
+class AliasFinderTest extends TestCase
+{
+    use CreateFunctionTrait;
+
+    /**
+     * @dataProvider functionProvider
+     *
+     * @param int                        $expected
+     * @param callable                   $aliasCallback
+     * @param ReflectionFunctionAbstract ...$functions
+     *
+     * @return void
+     *
+     * @covers ::__construct
+     * @covers ::__invoke
+     */
+    public function testInvoke(
+        int $expected,
+        callable $aliasCallback,
+        ReflectionFunctionAbstract ...$functions
+    ): void {
+        /** @var FunctionFormatterInterface|MockObject $originalFormatter */
+        $originalFormatter = $this->createMock(FunctionFormatterInterface::class);
+
+        /** @var FunctionFormatterInterface|MockObject $aliasFormatter */
+        $aliasFormatter = $this->createMock(FunctionFormatterInterface::class);
+
+        $subject = new AliasFinder($originalFormatter, $aliasFormatter);
+
+        $originalFormatter
+            ->expects(self::exactly(count($functions)))
+            ->method('__invoke')
+            ->with(self::isInstanceOf(ReflectionFunctionAbstract::class))
+            ->willReturnCallback(
+                function (ReflectionFunctionAbstract $function): string {
+                    return $function->getName();
+                }
+            );
+
+        $aliasFormatter
+            ->expects(self::exactly(count($functions)))
+            ->method('__invoke')
+            ->with(self::isInstanceOf(ReflectionFunctionAbstract::class))
+            ->willReturnCallback($aliasCallback);
+
+        $result = $subject->__invoke(...$functions);
+
+        $this->assertInstanceOf(AliasIteratorInterface::class, $result);
+        $this->assertCount($expected, iterator_to_array($result));
+    }
+
+    /**
+     * @return array
+     */
+    public function functionProvider(): array
+    {
+        return [
+            [
+                0,
+                function (): void {
+                    // Nothing to do.
+                }
+            ],
+            [
+                0,
+                function (ReflectionFunctionAbstract $function): string {
+                    return $function->getName();
+                },
+                $this->createFunction('in_array')
+            ],
+            [
+                0,
+                function (): string {
+                    return '';
+                },
+                $this->createFunction('in_array')
+            ],
+            [
+                1,
+                function (ReflectionFunctionAbstract $function): string {
+                    return md5($function->getName());
+                },
+                $this->createFunction('in_array')
+            ],
+            [
+                1,
+                function (ReflectionFunctionAbstract $function): string {
+                    return md5($function->getName());
+                },
+                $this->createFunction('in_array'),
+                $this->createFunction('in_array'),
+                $this->createFunction('in_array')
+            ],
+            [
+                2,
+                function (ReflectionFunctionAbstract $function): string {
+                    return md5($function->getName());
+                },
+                $this->createFunction('in_array'),
+                $this->createFunction('array_key_exists'),
+                $this->createFunction('in_array')
+            ]
+        ];
+    }
+}
